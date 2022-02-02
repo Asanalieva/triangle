13cd7cdd05733ac789046f152b57646ce4cf0394
Dilbara Asanalieva <dilbara2025@gmail.com>
2022-02-02 12:13:20 +0600

    Area of triangle

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Uchburchtuk bir = new Uchburchtuk();
+        Uchburchtuk eki = new Uchburchtuk();
+        Uchburchtuk uch = new Uchburchtuk();
+        System.out.print("Үч бурчтуктун аянты: ");
+        bir.area(4,5,6);
+    }
+}
---
+++ src/com/company/Uchburchtuk.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Uchburchtuk {
+    double a;
+    double b;
+    double c;
+
+    void area(double a, double b,double c){
+        System.out.println((a+b+c)/2);
+    }
+}
